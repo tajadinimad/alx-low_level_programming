41dea6775af5460096733007e5f9370bb1bf07d7
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-17 15:24:22 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,16 @@ int main(void)
 	int Alpha = 'a';
 
 	for (Alpha = 'a'; Alpha <= 'z'; Alpha++)
-		if (Alpha != 'q' ||  Alpha != 'e')
+	{
+		if (Alpha == 'q' ||  Alpha == 'e')
+		{
+			continue;
+		}
+		else 
 		{
 			putchar(Alpha);
 		}
+	}
 	putchar('\n');
 	return (0);
 }
