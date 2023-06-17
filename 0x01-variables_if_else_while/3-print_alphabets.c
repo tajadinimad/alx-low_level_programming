fdaf46ce3cba410479ee8f712846cd5a5db164b3
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-17 15:07:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int Alpha;
+
+	for (Alpha = 'a'; Alpha <= 'z'; Alpha++)
+		if (Alpha != 'q' ||  Alpha != 'e')
+		{
+			putchar(Alpha);
+		}
+	for (Alpha = 'A'; Alpha <= 'A'; Alpha++)
+                if (Alpha != 'Q' ||  Alpha != 'E')
+                {
+                        putchar(Alpha);
+                }
+	putchar('\n');
+	return (0);
+}
