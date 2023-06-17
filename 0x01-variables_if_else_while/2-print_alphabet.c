b9e69fabfe1fd33c217770fe4478cd305e12bbbc
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-17 14:59:30 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
-	int Alpha='a';
-	for (Alpha = 'a';Alpha <= 'z'; Alpha++)
-		if ( Alpha != 'q' ||  Alpha != 'e')
+	int Alpha = 'a';
+
+	for (Alpha = 'a'; Alpha <= 'z'; Alpha++)
+		if (Alpha != 'q' ||  Alpha != 'e')
 		{
 			putchar(Alpha);
 		}
 	putchar('\n');
 	return (0);
 }
-
