9ca857981e4dea78c18e51bcf6654740f144371e
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-17 15:43:11 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int Digit = 0;
+
+	for (Digit = 0; Digit <= 9; Digit++)
+	{
+		printf("%d", Digit);
+	}
+	putchar('\n');
+	return (0);
+}
