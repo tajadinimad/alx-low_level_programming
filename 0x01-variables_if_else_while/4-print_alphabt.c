af33cd25d8cd2fa4955a6813c9f6680b05932e6b
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-17 15:15:39 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int Alpha = 'a';
+
+	for (Alpha = 'a'; Alpha <= 'z'; Alpha++)
+		if (Alpha != 'q' ||  Alpha != 'e')
+		{
+			putchar(Alpha);
+		}
+	putchar('\n');
+	return (0);
+}
