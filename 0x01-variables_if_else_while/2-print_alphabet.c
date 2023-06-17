ce12ac44912701bd8852345b92fcc25967f1c3b0
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-17 14:56:15 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int Alpha='a';
+	for (Alpha = 'a';Alpha <= 'z'; Alpha++)
+		if ( Alpha != 'q' ||  Alpha != 'e')
+		{
+			putchar(Alpha);
+		}
+	putchar('\n');
+	return (0);
+}
+
