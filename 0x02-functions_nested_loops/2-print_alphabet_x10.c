fd4f044496a5351d2e24cb892a4a066a424f2b52
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-23 12:50:25 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+void print_alphabet_x10(void)
+{
+	int R = 1;
+
+	while (R < 10)
+	{
+		print_alphabet();
+		R++;
+	}
+}
