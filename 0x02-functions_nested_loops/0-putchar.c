1e241b728c553a3428a476fc24a9cd05102ae1af
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-23 07:30:17 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+/* function declaration 
+int max(int num1, int num2);*/
+ 
+int main () {
+
+   /* local variable definition *
+   int a = 100;
+   int b = 200;
+   int ret;
+ */
+   /* calling a function to get max value 
+   ret = max(a, b);*/
+ 
+   printf( "_putchar\n" );
+ 
+   return 0;
+}
