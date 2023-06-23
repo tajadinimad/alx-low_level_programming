0bd00093ceaca227e3e45858c5f1739beaa48742
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-23 13:19:26 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * int _islower - prints if a char is lowercase or not
+ *
+ * Return: 1 or 0;
+ *
+ */
+int _islower(int c)
+{
+	int result = islower(c);
+	return result;
+}
