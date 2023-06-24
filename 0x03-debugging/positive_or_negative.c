c36b21ab79fb8d716ca8e8711e0b7896da0883ef
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-24 10:38:32 -0700

    0. Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,10 +8,8 @@
  * Return:always return 0
  *
  */
-int postitive_or_negative(int n)
+void postitive_or_negative(int n)
 {
-	/*srand(time(0));
-	n = rand() - RAND_MAX / 2;*/
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
