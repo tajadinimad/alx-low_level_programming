46c0d000cf68831a6abd17b36953f7ce35090c31
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-24 01:55:06 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_sign - main function
+ *
+ * @n: character to compare
+ *
+ * Return: 1 if positive, -1 if negative , 0 if null
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+	else
+	{	
+		_putchar('-');
+		return (-1);
+	}
+}
