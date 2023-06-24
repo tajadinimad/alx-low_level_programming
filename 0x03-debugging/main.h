ef42b40731d30ff958881330592ae024dbc25ed1
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-24 10:20:57 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
