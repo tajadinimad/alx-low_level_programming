7d4988436e6996f7cfd5461b36320ac1dab449eb
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-24 01:57:24 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,10 +16,10 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n == 0)
-        {
-                _putchar('0');
-                return (0);
-        }
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
 	{	
 		_putchar('-');
