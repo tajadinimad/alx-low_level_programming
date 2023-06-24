6d1ababf84f73098963b8fbabf69f12e63805c97
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-24 11:05:22 -0700

    Correction of 2. 0 > 972?"

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
