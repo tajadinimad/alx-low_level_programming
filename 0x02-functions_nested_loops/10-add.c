c31e415bb7937a67b2cd580337c7be35da60a629
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-24 03:53:22 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* 
+ * add function
+ * @a,@b two integer as variables
+ * return the addition of two variable 
+ *
+ */
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
