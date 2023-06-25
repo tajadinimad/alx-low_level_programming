7c84f3e7e07de16026371eb323b05146339a060c
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-06-25 02:23:48 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - check if a letter is low case or uppercase
+ * left in the year, taking leap years into account
+ * @c: character to be tester
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{	
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int _isupper(int c);
+#endif /* MAIN_H */
