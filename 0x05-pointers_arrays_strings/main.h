dec7183a2d2975ede3e50d19ab0a41f8b302f1b9
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-07-03 04:06:51 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+void reset_to_98(int *n);
+#endif /* MAIN_H */
