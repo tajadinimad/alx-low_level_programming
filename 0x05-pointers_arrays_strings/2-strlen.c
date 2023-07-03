6523a680a6b4d699ac1a49b0b385ee509928e623
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-07-03 04:47:00 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - This report, by its very length,
+ * defends itself
+ *  against the risk of being read
+ *
+ * @s: pointer integer
+ */
+int _strlen(char *s)
+{
+	int c = strlen(s);
+
+	return (c);
+}
