a1bed0ed774a507aa9ff59f549b276b9d409b21e
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-07-03 05:44:05 -0700

    4. I can only go one way. I ve not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - function that prints a reversed string
+ * @s: pointer string
+ */
+void print_rev(char *s)
+{
+	int c = strlen(s);
+	int i;
+
+	for (i = c; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
