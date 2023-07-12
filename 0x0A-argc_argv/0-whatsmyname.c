3acd48f09cedc7a4ad239f401a1d357efba79ef2
Imad Taje EDDINE <tajadinimad@gmail.com>
2023-07-12 09:37:01 -0700

    0. It ain t what they call you, it s what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,11 @@
  * @argv: vector of arguments
  *Return: always 0
  */
-int main (int argc, char *argv[])
-{  
+int main(int argc, char *argv[])
+{
 	if (argc >= 1)
 	{
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
